d5b85476a806fd286b83a94ce651491c101362a4
MarleneLopez <lopez.mpln@gmail.com>
2019-01-09 12:52:14 -0300

    Agregada la funcion Sort, el Dom de Sort imprimiendo en CARDS y el Select de Sort en el HTML

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 //Filter
 const filterData = (data, condition) => {
   const filteredData = data.filter(element => {
-      return element.type.includes(condition) === true
+    return element.type.includes(condition) === true
   })
   return filteredData;
 }
@@ -10,10 +10,68 @@ window.filterData = filterData;
 
 
 //Sort
-const sortData = (data, sortBy, sortOrder) =>{
 
+
+
+// Metodo para ordrnar los pokemons alfabeticamente "a-z" o "z-a"
+// Por hacer: Orden numerico
+//Sort
+
+
+const sortData = (data, sortBy) => {
+  // console.log(data, sortOrder); 
+
+  if (sortBy == 'a-z') {
+
+    return data.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+
+  if (sortBy == 'z-a') {
+    return data.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+  if (sortBy == '001-151') {
+    return data.sort((a, b) => {
+      if (a.num > b.num) {
+        return 1;
+      }
+      if (a.num < b.num) {
+        return -1;
+      }
+      return 0;
+
+    });
+  }
+
+  if (sortBy == '151-001') {
+    return data.sort((a, b) => {
+      if (a.num < b.num) {
+        return 1;
+      }
+      if (a.num > b.num) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 
 }
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ const showData = (data) => {
 
 
     });
-    return result;  
+    return result;
 }
 
 //Filtrado, muestra en CARDS
@@ -53,6 +53,48 @@ selectType.addEventListener("change", () => {
         </div>
         </div> `
     })
+})
+
+
+
+
+document.getElementById('sort').addEventListener('change', (evento) => {
+    evento.preventDefault();
+
+    //Creo una nueva constante "s   eleccionOrden" donde tomo el valor seleccionado en Select
+    const sorted = document.getElementById('sort').value
+    //variable que  guarda todos los return de "ordenarPokemons" de la data.js
+    let ver = sortData(data, sorted)
+
+    //Se borra lo impreso cada vez que vuelvo a seleccionar otro ordenador, debe ser antes del que se imprime en la pantalla
+    document.getElementById("root").innerHTML = " ";
+
+    //se recorre la variable "ver" con todos los datos de "ordenado"
+    for (let i = 0; i < ver.length; i++) {
+
+        //Se imprime en el id=vacío, la variable ver siendo recorrida. Muestro solo el nombre    
+        document.getElementById("root").innerHTML += //ver[i].name + "<br>";
+
+            //'<div class ="col s10"      >' + '<h5>' + ver[i].num + '</h5>' + '<h3>' + ver[i].name + '</h3>' + '<img src="' + ver[i].img + '">' + '<p>' + ver[i].type + '</p>' + '</div>';   // mostrando el rsultado en array para diferentes keys del objeto.;
+
+
+            /*result = containerRoot.innerHTML += */`
+            <div>
+            <div class="card">
+            <div class="box">
+            <h3>${ver[i].num}</h3>
+                <div class="img">
+                    <img src="${ver[i].img}">
+                </div>
+                <h2>${ver[i].name}</h2>
+                <p>${ver[i].type}</p>
+            </div>
+            </div>
+            </div> `
+
+    }
+
+    // console.log(ver)
 })
 //Que se cargue solo esto en un principio
 window.onload = showData(data);
